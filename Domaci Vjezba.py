8cf43e7d1ff403a2f6a053c12b71f2bc66ed79f4
Vanja <vanjamne@gmail.com>
2019-11-09 17:23:03 +0100

    Domaci

---
+++ Domaci Vjezba.py
@@ -0,0 +1,33 @@
+# 4.Ako imate string “Prvi Domaći” kako da izdvojite slovo ć iz stringa?
+# Kako biste razbili string na dvije riječi [“Prvi”, “Domaći”]?
+# Kako biste našli indeks slova D, a kako biste uklonili slovo D?
+from typing import List, Union
+
+# word = "Prvi Domaci"
+#
+# print(len(word))
+# print(word[-2])
+# print(word.split(" "))
+# print(word[5])
+# print(word.replace( "D"," " ))
+
+# 5. U listi lista = [1, 2, [3, 4,”hello”]] promijeniti element čija je vrijednost “hello” u “hi”
+
+# list = [1, 2, [3, 4,"hello"]]
+
+
+# 6.Tražiti od korisnika ime I godine.
+# Koristite sve načine print-a da bi se dobio format: “Cao, ja sam ime_korisnika. Imam godine godina.”
+
+# Ime = input("Unesite Vase Ime:")
+# Godine = input("Unesite vase godine:")
+# Pozdrav = "Cao, ja sam "
+#
+# print(format(Pozdrav + Ime + '.Imam ' + Godine + " godina."))
+#
+#Boolean
+# print(bool([] == {}))
+# print(bool(3.0 == 3))
+# print(bool(4**0.5 == 2))
+
+
